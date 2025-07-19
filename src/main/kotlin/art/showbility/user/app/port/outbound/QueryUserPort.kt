38ed93f69b97fe77211246d4d37d54f874cf8b6f
Injoon Hwang <yuwol036@gmail.com>
2025-07-19 23:51:14 +0900

    Define user use cases and ports

---
+++ src/main/kotlin/art/showbility/user/app/port/outbound/QueryUserPort.kt
@@ -0,0 +1,10 @@
+package art.showbility.user.app.port.outbound
+
+import art.showbility.user.domain.User
+import art.showbility.user.domain.UserId
+
+interface QueryUserPort {
+    fun findById(id: UserId): User?
+
+    fun getById(id: UserId): User
+}
