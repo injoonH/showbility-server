4d2f8db2f9f3742757bfba5b51d9b94236a2f94a
Injoon Hwang <yuwol036@gmail.com>
2025-07-20 14:07:22 +0900

    Add user table creation script

---
+++ src/main/resources/db/migration/V20250713200817__create_user_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS "user"
+(
+    id         BIGSERIAL PRIMARY KEY,
+    handle     VARCHAR(32)                         NOT NULL,
+    nickname   VARCHAR(32)                         NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+ALTER TABLE "user"
+    ADD CONSTRAINT user_handle_unique UNIQUE (handle);
+CREATE
+    SEQUENCE IF NOT EXISTS user_id_seq START
+    WITH 1 MINVALUE 1 MAXVALUE 9223372036854775807;
